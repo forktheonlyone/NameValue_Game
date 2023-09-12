79806ee32caebdcd7b7623bb18d371c931aff6dd
stulss <doffl9737@gmail.com>
2023-09-12 10:06:13 +0900

    몬스터리스트수정(어레이->링크드)

--- Monster.java
+++ Monster.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Monster extends Charactor {
@@ -15,45 +16,57 @@ public class Monster extends Charactor {
     public void Attack(Charactor enemy) {
         int damage = atk - enemy.def;
         if(damage<1){damage=1;}
-        if(enemy.getHp() < damage){enemy.setHp(0);}
+        if(enemy.getHp() < damage){System.exit(0);}
         else {enemy.setHp(enemy.getHp() - damage);}
         System.out.println(getNickName()+"가"+enemy.getNickName()+"에게"+damage+"만큼 데미지를 주었습니다.");
         System.out.println(enemy.nickName+"의 현재 HP : "+enemy.hp);
     }
 
-    private List<Monster> monsterList = new ArrayList<>();
 
-    public Monster getMonsterList(int i) {
+    private List<Monster> monsterList = new LinkedList<>();
+
+
+    public Monster getmonsterList(int i) {
         return monsterList.get(i);
     }
 
-    protected void MonsterList()
+    public void remove(){monsterList.remove(0);}
+
+
+    protected void GoblinList()
     {
-
-
         // Monster creation
         monsterList.add(new Monster("고블린", 10, 1, 0, 9));
         monsterList.add(new Monster("고블린", 10, 1, 0, 9));
         monsterList.add(new Monster("고블린보스", 14, 2, 0, 9));
+    }
+
+    protected void OrkList()
+    {
+        // Monster creation
         monsterList.add(new Monster("오크", 12, 3, 0, 9));
         monsterList.add(new Monster("오크", 12, 3, 0, 9));
         monsterList.add(new Monster("오크보스", 26, 4, 0, 9));
+    }
+    protected void ElfList()
+    {
+        // Monster creation
         monsterList.add(new Monster("나이트엘프정찰병", 12, 5, 0, 9));
         monsterList.add(new Monster("나이트엘프정찰병", 12, 5, 0, 9));
         monsterList.add(new Monster("나이트엘프네임드", 15, 7, 0, 9));
         monsterList.add(new Monster("나이트엘프보스", 20, 8, 0, 9));
-        monsterList.add(new Monster("리치왕 아서스 메놀드", 92, 9, 1, 11));
-
-
-
-
-
-
-    /*    // Get a specific monster from the list and use it
-        Monster specificMonster = monsterList.get(1); // Get the second monster in the list (Goblin)
-
-        System.out.println(specificMonster.getNickName() + " was retrieved from the list.");*/
     }
+    protected void LastBoss()
+    {
+        // Monster creation
+        monsterList.add(new Monster("리치왕 아서스 메놀드", 92, 9, 1, 11));
+    }
+
+
+
+
+
+
 
 
 }
