99f6e47eaeba418b4eb37b0a01adfbd2f6f7d5e3
forktheonlyone <forktheonlyone@gmail.com>
2023-09-12 17:04:31 +0900

    여관 대장간 클래스 수정
    수정

--- GoSmith.java
+++ GoSmith.java
@@ -1,7 +1,26 @@
 // 작성자 : 송한올
 // 23.09.12
+// indev 0.01
 
 public class GoSmith {
+    private int[][] gear =
+            {
+                    {2, 0},
+                    {4, 1},
+                    {6, 2},
+                    {8, 3}
+            };
+    private int[][] upgradesGold =
+            {
+                    {50, 30},
+                    {100, 60},
+                    {150, 90}
+            };
+
+    private int swordUpLv = 0;
+    private int armorUpLv = 0;
+
+
 
     public void SmithMenu(Charactor chr)
     {
@@ -11,105 +30,65 @@ public class GoSmith {
         System.out.println("대장장이 : 도움이 필요한가?");
 
         Player player = new Player(chr.getNickName());
-        if (chr.getAtk() == 2 || chr.getDef() == 0)
-        {
-            System.out.println("1. 검을 강화 ( -50 Gold )");
-            System.out.println("2. 방어구를 강화 ( -30 Gold )");
-            System.out.println("3. 마을로 돌아간다.");
-            ScannerManager.Initialize();
-            switch (ScannerManager.Scan())
-            {
-                case 1 :
-                    if(player.getGold() >= 50)
-                    {
-                        player.setGold(player.getGold() -50);
-                        player.setAtk(4);
-                        System.out.println("강화에 성공하셨습니다!");
-                    }
-                    else
-                    {
-                        System.out.println("골드가 부족합니다.");
-                    }
-                case 2 :
-                    if(player.getGold() >= 30)
-                    {
-                        player.setGold(player.getGold() -30);
-                        player.setDef(1);
-                        System.out.println("강화에 성공하셨습니다!");
-                    }
-                    else
-                    {
-                        System.out.println("골드가 부족합니다.");
-                    }
-                case 3 :
 
-            }
-        }
-        else if (chr.getAtk() == 4 || chr.getDef() == 1)
+        boolean repeat = true;
+        while (repeat)
         {
-            System.out.println("1. 검을 강화한다. ( -100 Gold )");
-            System.out.println("2. 방어구를 강화한다. ( -60 Gold )");
-            ScannerManager.Initialize();
+            if (swordUpLv == 4 )
+            {
+                System.out.println("무기 강화 최대치");
+            }
+            else
+            {
+                System.out.println("1. 무기 강화");
+            }
+            if (armorUpLv == 4)
+            {
+                System.out.println("방어구 강화 최대치");
+            }
+            else
+            {
+                System.out.println("2. 방어구 강화");
+            }
+            System.out.println("3. 마을로 돌아가기");
             switch (ScannerManager.Scan())
             {
                 case 1 :
-                    if(player.getGold() >= 100)
+                    if (swordUpLv == 4)
                     {
-                        player.setGold(player.getGold() -100);
-                        player.setAtk(6);
-                        System.out.println("강화에 성공하셨습니다!");
+                        System.out.println("대장장이 : 내 능력으로는 더 이상 강화가 불가능하네.");
+                    }
+                    else if (player.getGold() >= upgradesGold[swordUpLv][0])
+                    {
+                        player.setGold(player.getGold() - upgradesGold[swordUpLv][0]);
+                        player.setAtk(gear[swordUpLv][0]);
+                        swordUpLv++;
                     }
                     else
                     {
                         System.out.println("골드가 부족합니다.");
                     }
+                    break;
                 case 2 :
-                    if(player.getGold() >= 60)
+                    if (armorUpLv == 4)
                     {
-                        player.setGold(player.getGold() -60);
-                        player.setDef(2);
-                        System.out.println("강화에 성공하셨습니다!");
+                        System.out.println("대장장이 : 내 능력으로는 더 이상 강화가 불가능하네.");
+                    }
+                    else if (player.getGold() >= upgradesGold[armorUpLv][1])
+                    {
+                        player.setGold(player.getGold() - upgradesGold[armorUpLv][1]);
+                        player.setDef(gear[armorUpLv][1]);
+                        armorUpLv++;
                     }
                     else
                     {
                         System.out.println("골드가 부족합니다.");
                     }
+                    break;
+                case 3 :
+                    repeat = false;
+                    break;
             }
-        }
-        else if (chr.getAtk() == 6 || chr.getDef() == 2)
-        {
-            System.out.println("1. 검을 강화한다. ( -150 Gold )");
-            System.out.println("2. 방어구를 강화한다. ( -90 Gold )");
-            ScannerManager.Initialize();
-            switch (ScannerManager.Scan())
-            {
-                case 1 :
-                    if(player.getGold() >= 150)
-                    {
-                        player.setGold(player.getGold() -150);
-                        player.setAtk(8);
-                        System.out.println("강화에 성공하셨습니다!");
-                    }
-                    else
-                    {
-                        System.out.println("골드가 부족합니다.");
-                    }
-                case 2 :
-                    if(player.getGold() >= 90)
-                    {
-                        player.setGold(player.getGold() -90);
-                        player.setDef(3);
-                        System.out.println("강화에 성공하셨습니다!");
-                    }
-                    else
-                    {
-                        System.out.println("골드가 부족합니다.");
-                    }
-            }
-        }
-        else
-        {
-            System.out.println("대장장이 : 내 능력으론 더 이상 강화가 불가능하네.");
         }
     }
 }
